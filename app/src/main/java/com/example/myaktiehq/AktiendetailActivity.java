cc8f387dd0c59342748ce2af07a15c40bc937f1a
Uli <post2uli@gmx.de>
2019-02-13 14:29:38 +0100

    android-activities-und-intents

--- app/src/main/java/com/example/myaktiehq/AktiendetailActivity.java
+++ app/src/main/java/com/example/myaktiehq/AktiendetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class AktiendetailActivity extends AppCompatActivity {
@@ -16,23 +17,18 @@ public class AktiendetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_aktiendetail, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings wurde gedrückt", Toast.LENGTH_SHORT);
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
