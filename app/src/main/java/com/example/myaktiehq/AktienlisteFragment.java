c052955855329050ebdd993db18a4a381138fbf6
ulrich1966 <post2uli@gmx.de>
2019-02-19 18:05:41 +0100

    refactoring

--- app/src/main/java/com/example/myaktiehq/AktienlisteFragment.java
+++ app/src/main/java/com/example/myaktiehq/AktienlisteFragment.java
@@ -1,15 +1,11 @@
 package com.example.myaktiehq;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,11 +30,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -93,12 +87,12 @@ public class AktienlisteFragment extends Fragment {
         Log.d(TAG, "onDestroy:");
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        Log.d(TAG, "onAttach: Wird aufgerufen, wenn das Fragment mit der Activity verbunden wurde");
-        Log.d(TAG, String.format("... Mit der Activity %s verbunden", activity.getClass().getName()));
-    }
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        Log.d(TAG, "onAttach: Wird aufgerufen, wenn das Fragment mit der Activity verbunden wurde");
+//        Log.d(TAG, String.format("... Mit der Activity %s verbunden", activity.getClass().getName()));
+//    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -119,7 +113,6 @@ public class AktienlisteFragment extends Fragment {
         Log.d(TAG, "onDetach: Wird aufgerufen, wenn die Verbindung zwischen Fragment und Activity gelöst wird");
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: Wird aufgerufen, um die View-Hierarchie des Fragments zu erstellen");
@@ -162,7 +155,7 @@ public class AktienlisteFragment extends Fragment {
          * fragment_aktienliste.xml hat z.B. eine android.widget.ListView mir der android id "@+id/frameLayout"
          * und kann hierüber referenziert werden.
          */
-        aktienlisteListView = (ListView) rootView.findViewById(R.id.listview_aktienliste);
+        aktienlisteListView = rootView.findViewById(R.id.listview_aktienliste);
         /**
          * Die Methode setAdapter() android.widget.ListView konsumiert eine Implementation vom des
          * Interfaces android.widget.ListAdapter. In diesm Fall also einen android.widget.ArrayAdapter
@@ -193,10 +186,6 @@ public class AktienlisteFragment extends Fragment {
          * zuweisen und dem SwipeRefreshLayout uebergeben.
          */
         mSwipeRefreshLayout.setOnRefreshListener(this::aktualisiereDaten);
-        /**
-         * Wenn die View aufgebaut ist noch ein paar aktuelle Daten hinzufuegen
-         */
-        //aktualisiereDaten();
         return rootView;
     }
 
@@ -209,7 +198,7 @@ public class AktienlisteFragment extends Fragment {
 
     /**
      * Bei der Erzeugung dieses Frgagments wird festgelegt, dass Menue-Events verarbeitet werden sollen
-     * @param savedInstanceState
+     * @param savedInstanceState Bundle
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -221,8 +210,8 @@ public class AktienlisteFragment extends Fragment {
     /**
      * Fuer das Menue soll als root-View die Datei menu_aktienfragment.xml mit der darin als Sub-View
      * (TextView) vorhandene action_daten_aktualisieren verwndenden.
-     * @param menu
-     * @param inflater
+     * @param menu Menu
+     * @param inflater MenuInflater
      */
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -233,7 +222,7 @@ public class AktienlisteFragment extends Fragment {
     /**
      * Aktualisieren der Daten ueber den Menueeintragen
      * @param item Das ausloesende Item
-     * @return
+     * @return true or false
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -261,7 +250,7 @@ public class AktienlisteFragment extends Fragment {
         String aktienliste = sPrefs.getString(prefAktienlisteKey, prefAktienlisteDefault);
 
         String prefIndizemodusKey = getString(R.string.preference_indizemodus_key);
-        Boolean indizemodus = sPrefs.getBoolean(prefIndizemodusKey, false);
+        boolean indizemodus = sPrefs.getBoolean(prefIndizemodusKey, false);
 
         if (indizemodus) {
             String indizeliste = "^GDAXI,^TECDAX,^MDAXI,^SDAXI,^GSPC,^N225,^HSI,XAGUSD=X,XAUUSD=X";
@@ -317,7 +306,7 @@ public class AktienlisteFragment extends Fragment {
             Log.d(TAG, "doInBackground: " + anfrageString);
             HttpURLConnection httpURLConnection = null;
             BufferedReader bufferedReader = null;
-            String aktiendatenXmlString = "";
+            StringBuilder sb = new StringBuilder();
             try {
                 URL url = new URL(anfrageString);
                 httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -328,12 +317,12 @@ public class AktienlisteFragment extends Fragment {
                 bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
-                    aktiendatenXmlString += line + "\n";
+                    sb.append(String.format("%s\n", line));
                 }
-                if (aktiendatenXmlString.length() == 0) {
+                if (sb.length() == 0) {
                     return null;
                 }
-                Log.d(TAG, "doInBackground: " + aktiendatenXmlString);
+                Log.d(TAG, "doInBackground: " + sb.toString());
                 publishProgress(1, 1);
             } catch (IOException e) {
                 Log.e(TAG, "doInBackground: Error ", e);
@@ -350,7 +339,7 @@ public class AktienlisteFragment extends Fragment {
                     }
                 }
             }
-            return leseXmlAktiendatenAus(aktiendatenXmlString);
+            return leseXmlAktiendatenAus(sb.toString());
         }
 
         @Override
